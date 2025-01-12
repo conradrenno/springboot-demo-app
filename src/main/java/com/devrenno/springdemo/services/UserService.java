c1cfaba5ce684c43ef946944ce105a4de4c460f3
conradrenno <conradrenno@hotmail.com>
2025-01-11 22:27:46 -0300

    Exception handling - update

--- src/main/java/com/devrenno/springdemo/services/UserService.java
+++ src/main/java/com/devrenno/springdemo/services/UserService.java
@@ -13,6 +13,8 @@ import com.devrenno.springdemo.repositories.UserRepository;
 import com.devrenno.springdemo.services.exceptions.DatabaseException;
 import com.devrenno.springdemo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, user);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, user);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User user) {
